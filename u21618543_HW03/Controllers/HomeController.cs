f0bdb963e1a5f2cbd036fb7c1da6af1cea63e6f1
21618543 <u21618543@tuks.co.za>
2022-06-08 23:09:29 +0200

    Coding

--- u21618543_HW03/Controllers/HomeController.cs
+++ u21618543_HW03/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
+
 
 namespace u21618543_HW03.Controllers
 {
@@ -14,6 +16,31 @@ namespace u21618543_HW03.Controllers
             return View();
         }
 
+        //Single File Upload
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase files) //INSIDE HOME
+        {
+            // Verify that the user selected a file
+            // Not null and has a length
+
+            if (files != null && files.ContentLength > 0)
+            {
+                // extract only the filename
+
+                var fileName = Path.GetFileName(files.FileName);
+
+                // store the file inside ~/App_Data/uploads folder
+
+                var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+
+                // The chosen default path for saving
+
+                files.SaveAs(path);
+            }
+            // redirect back to the index action to show the form once again
+
+            return RedirectToAction("Index");
+        }
         public ActionResult About()
         {
 
@@ -35,7 +62,7 @@ namespace u21618543_HW03.Controllers
 
             return View();
         }
-        public ActionResult Contact()
+        public ActionResult Files()
         {
 
             return View();
---
+++ u21618543_HW03/Models/HomeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace u21618543_HW03.Models
+{
+    public class HomeModel
+    {
+        public String Option { get; set; }
+
+        public HttpPostedFileBase[] Files { get; set; }
+    }
+}
