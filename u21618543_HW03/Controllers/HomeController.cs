cc0d35849abe2b1c57409b4802b0a8e2e9a069ed
21618543 <u21618543@tuks.co.za>
2022-06-08 02:39:28 +0200

    Sorting out navbar

--- u21618543_HW03/Controllers/HomeController.cs
+++ u21618543_HW03/Controllers/HomeController.cs
@@ -16,14 +16,13 @@ namespace u21618543_HW03.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }
