e79dcdf84f3480ca2d399fe56d3e90dea62677a4
21618543 <u21618543@tuks.co.za>
2022-06-09 23:19:33 +0200

    Extra code

--- u21618543_HW03/Models/FileModel.cs
+++ u21618543_HW03/Models/FileModel.cs
@@ -2,13 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
 
 namespace u21618543_HW03.Models
 {
     public class FileModel
     {
-        public String Option { get; set; }
+        [Display(Name = "File Name")]
+        public String FileName { get; set; }
 
+
+        [Required(ErrorMessage = "Please select file.")]
+        [Display(Name = "Browse File")]
         public HttpPostedFileBase[] Files { get; set; }
+
+
     }
 }
